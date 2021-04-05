0217969f2267a7b7f4e01a166c8cd471d1c0d607
Eldar <elogon@gmail.com>
2021-04-05 08:55:54 -0400

    Created Details and Experiences pages with routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-import Brazil from "../views/Brazil";
-import Hawaii from "../views/Hawaii";
-import Jamaica from "../views/Jamaica";
-import Panama from "../views/Panama";
 
 Vue.use(VueRouter);
 
@@ -12,40 +8,33 @@ const routes = [
   {
     path: "/",
     name: "Home",
+    props: true,
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: "/destination/:slug",
+    name: "DestinationDetails",
+    props: true,
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-  {
-    path: "/brazil",
-    name: "Brazil",
-    component: Brazil,
-  },
-  {
-    path: "/hawaii",
-    name: "Hawaii",
-    component: Hawaii,
-  },
-  {
-    path: "/jamaica",
-    name: "Jamaica",
-    component: Jamaica,
-  },
-  {
-    path: "/panama",
-    name: "Panama",
-    component: Panama,
+      import(
+        /* webpackChunkName: "DestiantionDetails" */ "../views/DestinationDetails"
+      ),
+    children: [
+      {
+        path: ":experienceSlug",
+        name: "experienceDetails",
+        props: true,
+        component: () =>
+          import(
+            /* webpackChunkName: "ExperienceDetails" */ "../views/ExperienceDetails"
+          ),
+      },
+    ],
   },
 ];
 
 const router = new VueRouter({
+  mode: "history",
   routes,
 });
 
